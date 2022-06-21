1e37925eca46020210a1a92bd24acf1cd60aad3a
Rúben ferreira <rubenferreira812@gmail.com>
2022-06-21 12:56:24 +0100

    Changes to menu and design changes

--- js/views/menuView.js
+++ js/views/menuView.js
@@ -1,30 +1,56 @@
 import * as categories from "../models/CategoryModel.js";
 import * as wikiView from "../views/wikiView.js";
+import * as User from "../models/UserModel.js";
 
 let selectedSubCategory;
 let selectedCategory;
 
 menuView()
-function menuView()
-{
+function menuView() {
     categories.init();
+    User.init()
 
     let totalCategories = categories.getCategories();
 
     let categoriesDiv = document.querySelector(".categoriesDiv")
-
     for (let i = 0; i < totalCategories.length; i++) {
-        categoriesDiv.innerHTML += `<p class="levelNeeded">Level ${totalCategories[i].levelNeeded}</p>`
-        categoriesDiv.innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
-        totalCategories[i].subCategories.forEach(subCategory => {
-            categoriesDiv.innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
-        });
+
+        if (totalCategories[i].levelNeeded == 1) {
+            categoriesDiv.children[0].innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
+            totalCategories[i].subCategories.forEach(subCategory => {
+                categoriesDiv.children[0].innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
+            });
+        }
+        else if (totalCategories[i].levelNeeded == 2) {
+            categoriesDiv.children[1].innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
+            totalCategories[i].subCategories.forEach(subCategory => {
+                categoriesDiv.children[1].innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
+            });
+        }
+        else if (totalCategories[i].levelNeeded == 3) {
+            categoriesDiv.children[2].innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
+            totalCategories[i].subCategories.forEach(subCategory => {
+                categoriesDiv.children[2].innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
+            });
+        }
+        else if (totalCategories[i].levelNeeded == 4) {
+            categoriesDiv.children[3].innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
+            totalCategories[i].subCategories.forEach(subCategory => {
+                categoriesDiv.children[3].innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
+            });
+        }
+        else if (totalCategories[i].levelNeeded == 5) {
+            categoriesDiv.children[4].innerHTML += `<p class="categoryNotSelected">${totalCategories[i].title}</p>`
+            totalCategories[i].subCategories.forEach(subCategory => {
+                categoriesDiv.children[4].innerHTML += `<div><p class="subCategoryNotSelected">${subCategory.title}</p></div>`
+            });
+        }
+
         selectedSubCategory = totalCategories[0].subCategories[0]
         selectedCategory = totalCategories[0]
         updateData()
-        if(location.href.includes("wiki"))
-        {
-        wikiView.updateData()
+        if (location.href.includes("wiki")) {
+            wikiView.updateData()
         }
     }
 
@@ -45,42 +71,63 @@ function menuView()
             subCategory.classList.replace("subCategoryNotSelected", "subCategorySelected")
         }
         subCategory?.addEventListener("click", () => {
-            //Cleaning prev selected subCategories
-            totalSubCategories.forEach(subCategorySelected => {
-
-                subCategorySelected.classList.replace("subCategorySelected", "subCategoryNotSelected")
-            });
-            totalCategoriesHtml.forEach(categorySelected => {
-
-                categorySelected.classList.replace("categorySelected", "categoryNotSelected")
-            });
-
-            //Find the subCategory object with the same title
-            selectedSubCategory = categories.getSubCategoryByName(subCategory.innerHTML)
             selectedCategory = categories.checkWhereSubCategoryIs(subCategory.innerHTML)
-            updateData()
-            if(location.href.includes("wiki"))
-            {
-                wikiView.updateData()
-            }
-            subCategory.classList.replace("subCategoryNotSelected", "subCategorySelected")
-            totalCategoriesHtml.forEach(category => {
-                if (category.innerHTML == selectedCategory.title) {
-                    category.classList.replace("categoryNotSelected", "categorySelected")
+            if (selectedCategory.levelNeeded <= User.getUserLogged().level) {
+                //Cleaning prev selected subCategories
+                totalSubCategories.forEach(subCategorySelected => {
+
+                    subCategorySelected.classList.replace("subCategorySelected", "subCategoryNotSelected")
+                });
+                totalCategoriesHtml.forEach(categorySelected => {
+
+                    categorySelected.classList.replace("categorySelected", "categoryNotSelected")
+                });
+                //Find the subCategory object with the same title
+                selectedSubCategory = categories.getSubCategoryByName(subCategory.innerHTML)
+                updateData()
+                if (location.href.includes("wiki")) {
+                    wikiView.updateData()
                 }
-            });
+                subCategory.classList.replace("subCategoryNotSelected", "subCategorySelected")
+                totalCategoriesHtml.forEach(category => {
+                    if (category.innerHTML == selectedCategory.title) {
+                        category.classList.replace("categoryNotSelected", "categorySelected")
+                    }
+                });
+            }
+            else {
+                // Get the modal
+                let modal = document.getElementById("myModalLevel");
+
+                // Get the <span> element that closes the modal
+                let span = document.getElementsByClassName("closeLevel")[0];
+
+                let results = document.querySelector(".levelNeededModal")
+
+                results.innerHTML = `You need level ${selectedCategory.levelNeeded} to access this category`
+
+                modal.style.display = "block";
+
+                span.onclick = function () {
+                    modal.style.display = "none";
+                }
+
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+            }
         });
     });
     updateData()
-    if(location.href.includes("wiki"))
-    {
+    if (location.href.includes("wiki")) {
         wikiView.updateData()
     }
 
 }
 
-function updateData()
-{
+function updateData() {
     sessionStorage.setItem("selectedSubCategory", JSON.stringify(selectedSubCategory));
     sessionStorage.setItem("selectedCategory", JSON.stringify(selectedCategory));
 }
